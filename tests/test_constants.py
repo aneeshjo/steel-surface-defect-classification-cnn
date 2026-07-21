d2411ca76d618b0e3b791f7ff6a7e4f828b5eb76
aneeshjo <aneeshjose012@gmail.com>
2026-07-21 13:54:05 +0530

    feat: add project constants module

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,7 +1,12 @@
 from defect_detection.constants import *
 
-print(ROOT_DIR)
-print(CONFIG_FILE_PATH)
-print(PARAMS_FILE_PATH)
-print(SCHEMA_FILE_PATH)
-print(ARTIFACTS_DIR)
+print("ROOT_DIR       :", ROOT_DIR)
+print("CONFIG         :", CONFIG_FILE_PATH)
+print("PARAMS         :", PARAMS_FILE_PATH)
+print("SCHEMA         :", SCHEMA_FILE_PATH)
+print("ARTIFACTS      :", ARTIFACTS_DIR)
+print("DATA           :", DATA_DIR)
+print("LOGS           :", LOGS_DIR)
+print("MODELS         :", MODELS_DIR)
+print("REPORTS        :", REPORTS_DIR)
+print("NOTEBOOKS      :", NOTEBOOKS_DIR)
